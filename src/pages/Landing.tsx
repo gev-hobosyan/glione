e35d38b279297ce0cf9c65e56afef48238765fcb
Argo <hobosyangevorg2008@gmail.com>
2026-02-23 19:34:35 +0400

    Added GridScan.

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,32 @@
-// import { GridScan } from "@/components/GridScan";
-
-import Navbar from "../components/Navbar";
+import { GridScan } from "@/components/GridScan";
 
 const Landing = () => {
 	return (
 		<>
-			<div className="flex">
-				<Navbar />
+			<div
+				style={{
+					width: window.innerWidth,
+					height: window.innerHeight,
+					position: "relative",
+				}}
+			>
+				<GridScan
+					sensitivity={0.55}
+					lineThickness={1}
+					linesColor="#006E2A"
+					scanColor="#56AD2D"
+					scanOpacity={0.4}
+					gridScale={0.1}
+					lineStyle="solid"
+					lineJitter={0.2}
+					scanDirection="pingpong"
+					noiseIntensity={0.01}
+					scanGlow={0.6}
+					scanSoftness={2.5}
+					scanDuration={2.5}
+					scanDelay={0.5}
+					scanOnClick={false}
+				/>
 			</div>
 		</>
 	);
