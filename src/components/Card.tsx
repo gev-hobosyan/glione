7a9d7e52ceb29b7cf48fa10d4fff43057384f7ca
Argo <hobosyangevorg2008@gmail.com>
2026-03-02 13:08:16 +0400

    Gradiend Spining border.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import { MoveRight } from "lucide-react";
+import React from "react";
 
 interface Props {
 	index: number;
@@ -10,19 +11,23 @@ interface Props {
 const Card = ({ title, description, index, text }: Props) => {
 	return (
 		<>
-			<div className="w-82 rounded-2xl border border-white/5 px-10 py-5 hover:-translate-y-3 transition-all duration-300">
-				<p className="text-secondary text-[10px]">
-					{index >= 10 ? index : `0${index}`} / {text}
-				</p>
-				<h4 className="mt-3 text-[15px]  text-white">{title}</h4>
-				<p className="mt-3 text-[12px] text-white">{description}</p>
-				<div className="gap-3 mt-5 flex items-center group cursor-pointer">
-					<h6 className="text-xs text-secondary">View Details</h6>
-					<MoveRight className="stroke-secondary w-4 group-hover:translate-x-1 transition-all duration-150" />
+			<div
+				className={`card-wrapper w-90 h-45 hover:-translate-y-3 transition-all duration-300`}
+			>
+				<div className="card-content w-82 rounded-2xl border border-white/5 px-10 py-5 flex flex-col justify-center">
+					<p className="text-secondary text-[10px]">
+						{index >= 10 ? index : `0${index}`} / {text}
+					</p>
+					<h4 className="mt-3 text-[15px]  text-white">{title}</h4>
+					<p className="mt-3 text-[12px] text-white">{description}</p>
+					<div className="gap-3 mt-5 flex items-center group cursor-pointer">
+						<h6 className="text-xs text-secondary">View Details</h6>
+						<MoveRight className="stroke-secondary w-4 group-hover:translate-x-1 transition-all duration-150" />
+					</div>
 				</div>
 			</div>
 		</>
 	);
 };
 
-export default Card;
+export default React.memo(Card);
