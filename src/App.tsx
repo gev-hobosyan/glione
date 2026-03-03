fe26d52ea24ca938d59a41518b821bb8e2e8c45f
Levon Avetisyan <levonavetisyan@Levons-MacBook-Air.local>
2026-03-03 17:58:53 +0400

    Added Sign Up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Landing />} />
 					<Route path="/signin" element={<SignIn/>} />
+					<Route path="/signup" element={<SignUp/>} />
 				</Routes>
 			</BrowserRouter>
 		</>
