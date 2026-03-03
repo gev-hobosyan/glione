07921a47f29cde094b1e18baddfdae89b86440cc
Levon Avetisyan <levonavetisyan@Levons-MacBook-Air.local>
2026-03-03 18:47:53 +0400

    Change Sign Up inputs

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Landing />} />
 					<Route path="/login" element={<SignIn />} />
+					<Route path="/signup" element={<SignUp />} />
 				</Routes>
 			</BrowserRouter>
 		</>
