ae106ab3fb6e4a40c4379a0d8a20c0b38bd5fb09
Argo <hobosyangevorg2008@gmail.com>
2026-03-04 10:45:19 +0400

    Linked all the pages.

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,13 +1,22 @@
+import { ArrowLeft, Check, CheckCircle, CheckCircle2 } from "lucide-react";
 import Input from "@/components/Input";
 import React from "react";
 import LoginOptions from "@/components/LoginOptions";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
 	return (
 		<div className="h-screen w-screen overflow-hidden flex items-center justify-center">
+			{
+				// Not the final version. Might be removed
+			}
+			<Link to="/">
+				<ArrowLeft className="stroke-secondary fixed top-7 left-7 w-8 hover:scale-110 transition-all duration-300" />
+			</Link>
+
 			<div className="flex flex-col items-center justify-center h-screen w-[50%] border-r border-r-gray-500 rounded-2xl">
-				<img src="/icon.svg" className="w-20 h-20" />
-				<p className="text-white text-2xl mb-20">Welcome Back!</p>
+				<img src="/icon.svg" className="w-20 h-20 mb-1" />
+				<p className="text-white text-2xl mb-7">Welcome Back!</p>
 				<form className="flex flex-col gap-3">
 					<Input id="email" type="email">
 						Email
@@ -22,6 +31,12 @@ const SignIn = () => {
 					></input>
 				</form>
 				<LoginOptions />
+				<div className="mt-10 flex items-center justify-center gap-1.5 text-[14px]">
+					<p className="text-white">Don't have an account?</p>
+					<Link className="text-secondary" to="/signup">
+						Sign Up
+					</Link>
+				</div>
 			</div>
 			<div className="h-screen w-[50%]">
 				<p className="text-white flex items-center justify-center mt-60 mr-80 text-[24px]">
