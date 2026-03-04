bb3005a2f1e6868008af3b8d6973a36d41106405
Argo <hobosyangevorg2008@gmail.com>
2026-03-04 10:34:09 +0400

    Added login options and some animations

---
+++ src/components/LoginOptions.tsx
@@ -0,0 +1,18 @@
+const LoginOptions = () => {
+	return (
+		<>
+			<div className="mt-4 flex items-center justify-center w-60">
+				<div className="bg-gray-500 h-[1.5px] w-full"></div>
+				<span className="text-gray-500 text-sm mx-3">or</span>
+				<div className="bg-gray-500 h-[1.5px] w-full"></div>
+			</div>
+			<div className="mt-5">
+				<div className="text-white text-[16px] flex items-center justify-center gap-2 border border-white rounded-xl px-10 py-2.5 cursor-pointer hover:scale-105 transition-all duration-300">
+					Log in with <img src="/google.png" className="w-5" />
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default LoginOptions;
