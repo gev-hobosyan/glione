d2b4eba5a8f6f2682ca8cfdf629a8614de5be32f
Argo <hobosyangevorg2008@gmail.com>
2026-03-08 22:57:17 +0400

    CodeEditor custom theme.

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,44 +1,89 @@
-import Editor from "@monaco-editor/react";
-import { type Dispatch } from "react";
+import Editor, { useMonaco } from "@monaco-editor/react";
+import { useEffect, useState, type Dispatch } from "react";
 import { Pyodide } from "../lib/pyodide.js";
-
-interface Props {
-	value: string;
-	setValue: Dispatch<React.SetStateAction<string>>;
-}
+import { PlayIcon } from "lucide-react";
 
 /**
  * The CodeEditor used in the project
  *
- * @param {string} value Store the code in this variable
- * @param {Dispatch} setValue The react useEffect hook function to change the value
- *
  */
-const CodeEditor = ({ value, setValue }: Props) => {
+const CodeEditor = () => {
+	const [value, setValue] = useState(`class Player:
+    def __init__(self, coins):
+        self.coins = coins
+
+    def add_coins(self, coins):
+        self.coins += coins
+
+player = Player(10)
+
+print(player.coins)
+
+player.add_coins(2)
+
+print(player.coins)`);
 	const pyodide = Pyodide.getInstance();
 
+	const monaco = useMonaco();
+
+	useEffect(() => {
+		if (monaco) {
+			monaco.editor.defineTheme("myTheme", {
+				base: "vs-dark",
+				inherit: true,
+				rules: [
+					{ token: "string", foreground: "#56AD2D" },
+					{ token: "comment", foreground: "#6D8461" },
+					{ token: "variable", foreground: "#000000" },
+					{ token: "number", foreground: "#09885A" },
+					{ token: "keyword", foreground: "#09885A" },
+				],
+				colors: {
+					"editor.background": "#101010",
+					"editorCursor.foreground": "#56AD2D",
+					"editor.foreground": "#ffffff",
+					"editor.errorForeground": "#BF170E",
+					"editor.lineHighlightBorder": "#101010",
+					"editor.selectionBackground": "#404040",
+					"editor.lineHighlightBackground": "#242424",
+					"editorLineNumber.foreground": "#56AD2D",
+					"editorLineNumber.activeForeground": "#006E2A",
+				},
+			});
+			monaco.editor.setTheme("myTheme");
+		}
+	}, [monaco]);
+
 	return (
-		<>
+		<div className="border border-white rounded-4xl px-6 py-15 relative">
 			<Editor
 				height="50vh"
 				language="python"
 				theme="vs-dark"
-				width="80vh"
+				width="90vh"
 				value={value}
 				onChange={(value) => {
 					setValue(value!);
 					console.log(value);
 				}}
+				options={{
+					fontSize: 18,
+					fontFamily: "JetBrains Mono",
+					fontLigatures: true,
+					bracketPairColorization: {
+						enabled: false,
+					},
+				}}
 			/>
-			<button
-				className="text-white"
+			<PlayIcon
+				className="stroke-secondary fill-secondary absolute top-5 right-10"
 				onClick={() => {
 					pyodide.run(value);
 				}}
 			>
 				Run
-			</button>
-		</>
+			</PlayIcon>
+		</div>
 	);
 };
 
