0d4e1a1af3c84c207f4cb7a06e04b069f28cb0c8
gev-hobosyan <hobosyangevorg2008@gmail.com>
2026-03-30 21:24:20 +0400

    Finalized Choice element.

--- src/components/Choice.tsx
+++ src/components/Choice.tsx
@@ -1,15 +1,23 @@
 interface Props {
-    children: string;
-    isRight: boolean;
+	children: string;
+	isRight: boolean;
 }
-
 
 const Choice = ({ children, isRight }: Props) => {
-    return (
-        <div className="border border-primary/40 h-12 w-[calc(100%-100px)] flex items-center px-2">
-            <input type="radio" className="appearance-none border border-white rounded-full w-4 h-4 transition-all checked:bg-primary hover:shadow-effective" />
-            <p className="text-white ml-3">{children}</p>
-        </div>
-    )
-}
+	return (
+		<>
+			<div className="border border-primary w-[calc(100%-100px)] px-5 py-1.5 rounded-lg flex items-center gap-5 has-checked:shadow-[0_0_15px_#006e2a]">
+				<input
+					id={children}
+					type="radio"
+					name="choice"
+					className="appearance-none border border-white w-4 h-4 rounded-full relative checked:after:absolute checked:after:bg-primary checked:after:z-2 after:w-2.5 after:h-2.5 checked:after:rounded-full checked:after:top-0.5 checked:after:left-0.5 after:scale-0 checked:after:scale-100 transition-transform duration-300"
+				/>
+				<label htmlFor={children} className="text-white">
+					{children}
+				</label>
+			</div>
+		</>
+	);
+};
 export default Choice;
--- src/components/MultipleChoice.tsx
+++ src/components/MultipleChoice.tsx
@@ -1,36 +1,39 @@
-import Choice from "./Choice"
-
+import Choice from "./Choice";
 
 const MultipleChoice = () => {
-    const choices = [
-        {
-            text: "int x = 5",
-            isRight: true,
-        },
-        {
-            text: "x == 5",
-            isRight: false,
-        },
-        {
-            text: "declare x = 5",
-            isRight: false,
-        },
-        {
-            text: "x := int(5)",
-            isRight: false,
-        },
-    ]
-    return (
-        <>
-            <div className="w-full flex flex-col items-center justify-center ml-5">
-                <p className="text-green-700/70 mb-4 place-self-start">Which of the following is a correct way to create a variable in Python</p>
-                <form className="w-full ">
-                    {choices.map((choice) => <Choice isRight={choice.isRight}>{choice.text}</Choice>)}
-                </form>
-            </div>
-
-        </>
-    )
-}
+	const choices = [
+		{
+			text: "int x = 5",
+			isRight: true,
+		},
+		{
+			text: "x == 5",
+			isRight: false,
+		},
+		{
+			text: "declare x = 5",
+			isRight: false,
+		},
+		{
+			text: "x := int(5)",
+			isRight: false,
+		},
+	];
+	return (
+		<>
+			<div className="w-full flex flex-col items-center justify-center ml-5">
+				<p className="text-green-700/70 mb-4 place-self-start">
+					Which of the following is a correct way to create a variable in
+					Python
+				</p>
+				<div className="flex w-full items-center gap-4 flex-col">
+					{choices.map((choice) => (
+						<Choice isRight={choice.isRight}>{choice.text}</Choice>
+					))}
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default MultipleChoice;
