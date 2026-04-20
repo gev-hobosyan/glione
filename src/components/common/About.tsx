1bb5615a98686b767fb0a828165380e36f91ddad
nareazatyan08-bit <nare.azatyan08@gmail.com>
2026-04-20 17:39:23 +0400

    Added Armenian language to landing page

--- src/components/common/About.tsx
+++ src/components/common/About.tsx
@@ -1,4 +1,5 @@
 import BlurCircle from "@/components/common/BlurCircle";
+import { t } from "i18next";
 
 const About = () => {
 	return (
@@ -9,20 +10,15 @@ const About = () => {
 			<BlurCircle z="z-0" left="0" top="100px" />
 			<div className="w-1/2 ">
 				<h3 className="text-secondary text-[24px] mb-3 italic font-light">
-					Venture into the depths. Conquer the code. Master Python.
+					{t("AboutTitle")}
 				</h3>
 				<p className="mt-3 mb-3">
-					Glione is an interactive Python and Computer Science learning
-					platform where every lesson is a dungeon, every challange is a
-					monster.
+					{t("AboutDesc")}
 				</p>
 				<p className="mt-3 mb-3">
-					<span className="text-primary/60 font-semibold text-lg">What is Glione?</span>
+					<span className="text-primary/60 font-semibold text-lg">{t("AboutQuestion")}</span>
 					<br />
-					Glione turns learning Python into an adventure. Instead of dry
-					tutorials you explore dungoens, slaying monsters. Solve coding
-					challenges to defeat enemies, collect loot, and level up your
-					character as your real-world skills grow.
+					{t("AboutDesc1")}
 				</p>
 			</div>
 			<div className="w-1/2 flex items-center justify-center relative">
