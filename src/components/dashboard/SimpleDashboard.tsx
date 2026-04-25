7e8667aa36c7d479c35529aa36b8cbe7a44566a2
Natokma <Natali.Tokmajyan.2008@gmail.com>
2026-04-25 17:25:42 +0400

    Added responsiveness to dashboard

--- src/components/dashboard/SimpleDashboard.tsx
+++ src/components/dashboard/SimpleDashboard.tsx
@@ -3,23 +3,23 @@ import type { ReactNode } from "react";
 
 interface Props {
     number: string;
-	text: string;
+    text: string;
     icon: ReactNode;
     className?: string
 }
 
 
 const SimpleDashboard = ({ number, text, icon, className = "" }: Props) => {
-	return (
-        <div className={`border border-primary w-60 h-35 rounded-xl mt-5 ${className}`}>
-            <div className="flex ml-1">
-                <p className="text-white ml-2 mt-1 text-lg">{number}</p>
+    return (
+        <div className={`border border-primary py-5 px-6 w-full rounded-xl ${className}`}>
+            <div className="flex">
+                <p className="text-white text-lg">{number}</p>
                 {icon}
             </div>
-            <p className="text-gray-200 ml-2">{text}</p>
+            <p className="text-gray-200">{text}</p>
         </div>
-)
-} 
+    )
+}
 
 
 export default SimpleDashboard;
